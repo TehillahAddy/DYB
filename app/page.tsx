76fc02306b329513dd789e2a90e6a66039203f15
TehillahAddy <addyehillah@gmail.com>
2025-01-16 08:23:54 +0000

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Bar, Pie, Line, Bubble } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
